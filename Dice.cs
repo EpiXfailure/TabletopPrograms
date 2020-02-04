e3e1da55e4f6bdbb3340a4ddf79f82d445f19e1b
Sam Nguyen <samtnguyen@msn.com>
2020-02-03 21:47:35 -0800

    Make build project. Fixed Dice.cs to build

--- Dice.cs
+++ Dice.cs
@@ -3,56 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-/// <summary>
-/// Summary description for Class1
-/// </summary>
-public class DiceRoller 
+
+namespace TabletopPrograms
 {
-	public DiceRoller()
-	{
-        #DiceRoller will roll dice and log results
-        static List<Int32> dicelog = new List<Int32>();
-        private double average = 0;
+    public class DiceRoller 
+    {
+        private double avg = 0;
         private int highval = 0;
-        private int sum = 0;
+        private int total = 0;
+        private List<Int32> dicelog = new List<Int32>();
         
-	}
-    public int list_success(int limit)
-    {
-        #limit is the limit threshold.
-        #This function will return a list of values of succesfful dice rolls  
-        int sum = 0;
-        int dice_thrown = dicelog.Count;
-        int success = 0;
-        List<Int32> succeses = new List<Int32>
-        foreach(int rollvalue in self.dicelog)
+        public DiceRoller()
         {
-            int rollvalue = dicelog.Pop();
-            sum += rollvalue;
-            if (rollvalue >= limit)
-            {
-                successes.Add(rollvalue)
-                success++;
-            }
+            //DiceRoller will roll dice and log results
         }
-        return successes;
-    }
-    public void average()
-    {
-        self.avg = dicelog.Average();
-    }
-    public int highest()
-    {
-        self.high = dicelog.Max();
-    }
-    public void roll_dice(int type, int roll_count)
-    {
-        Random roll = new Random();
-        for (int index = 0; index < roll_count; index++)
+
+        public List<Int32> list_success(int limit)
         {
-            int rolledNum = roll.Next(1,type);
-            dicelog.Add(rolledNum);
-            //Console.WriteLine("Rolled: " + rolledNum);
+            //limit is the limit threshold.
+            //This function will return a list of values of succesfful dice rolls  
+            int dice_thrown = dicelog.Count;
+            int success = 0;
+            List<Int32> succeses = new List<Int32>();
+            foreach(int rollvalue in dicelog)
+            {
+                if (rollvalue >= limit)
+                {
+                    succeses.Add(rollvalue);
+                    success++;
+                }
+            }
+            return succeses;
+        }
+
+        public double average()
+        {
+            return avg;
+        }
+
+        public int highest()
+        {
+            return highval;
+        }
+        public int sum()
+        {
+            return total;
+        }
+
+        public void roll_dice(int type, int roll_count)
+        {
+            Random roll = new Random();
+            for (int index = 0; index < roll_count; index++)
+            {
+                int rolledNum = roll.Next(1,type);
+                dicelog.Add(rolledNum);
+                //Console.WriteLine("Rolled: " + rolledNum);
+            }
+            avg = dicelog.Average();
+            highval = dicelog.Max();
+            total = dicelog.Sum();
         }
     }
 }
